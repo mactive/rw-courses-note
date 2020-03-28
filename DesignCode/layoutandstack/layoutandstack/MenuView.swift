3dd0532dac52eb17f9369d38e138369f57cc0533
mactive <mactive@gmail.com>
2020-03-29 07:59:15 +0800

    26.09. Color Literal, Gradient and Overlay

--- DesignCode/layoutandstack/layoutandstack/MenuView.swift
+++ DesignCode/layoutandstack/layoutandstack/MenuView.swift
@@ -14,16 +14,35 @@ struct MenuView: View {
             Spacer()
 
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
                 MenuRow(icon: "gear", label: "Account")
                 MenuRow(icon: "creditcard", label: "Billing")
                 MenuRow(icon: "location.circle", label: "Location")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.7321143058, green: 0.8096149523, blue: 0.9197489767, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                .offset(y: -150)
+            )
         }
     }
 }
@@ -43,6 +62,7 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light, design: .rounded))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
         
             Text(self.label)
                 .font(.system(size: 20, weight: .bold, design: .rounded))
