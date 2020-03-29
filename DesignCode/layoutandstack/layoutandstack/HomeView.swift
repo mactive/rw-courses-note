202a3d9edbca43d1cbac2ec2b5f9cc0267272b59
mactive <mactive@gmail.com>
2020-03-29 10:10:09 +0800

    26.11. Drag Progress and Tap Background rotation3DEffect 性能火葬场

--- DesignCode/layoutandstack/layoutandstack/HomeView.swift
+++ DesignCode/layoutandstack/layoutandstack/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -40,20 +41,33 @@ struct HomeView: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset( y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
             .scaleEffect(self.showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
-            .onTapGesture(count: 1) {
-                self.showProfile.toggle()
-            }
 
             
             // 菜单层
             MenuView()
                 .offset(y: self.showProfile ? 0 : 600)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction:0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+            }
         }
+        // 作用在整个容器上
+        .gesture(
+            DragGesture().onChanged { value in
+                self.viewState = value.translation
+            }
+            .onEnded { value in
+                if self.viewState.height > 50 {
+                    self.showProfile = false
+                }
+                self.viewState = .zero
+            }
+        )
     }
 }
 
